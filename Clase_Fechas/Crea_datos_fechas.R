b3bfcddc5db6d78e39e7e067e232526a221aeb64
Jels95 <jelorias95@gmail.com>
2018-11-04 17:39:13 -0600

    Se agregan modificaciones y correcciones a la clase de fechas, además se modificaron los datos que se usan de insumo pues antes eran demasiado grandes e incómoodos

--- Clase_Fechas/Crea_datos_fechas.R
+++ Clase_Fechas/Crea_datos_fechas.R
@@ -7,7 +7,7 @@ set.seed(2345)
 Naci <- seq(as.Date('1900/01/01'), as.Date('1905/12/31'), by="day")
 Muerte <- seq(as.Date('1946/01/01'), as.Date('1990/12/31'), by="day")
 prob_muerte <- rev(cumsum(rep(1,16436)))
-Nacimientos <- data.frame(ID = 1:31243) %>% 
+Nacimientos <- data.frame(ID = 1:3103) %>% 
   rowwise() %>% 
   mutate(Fecha = as.character(sample(Naci, 1,prob = c(rep(1,800),200,rep(2,1390))))) %>% 
   mutate(Hora_Nac = sample(24,size = 1,replace = FALSE)-1) %>% 
@@ -22,17 +22,26 @@ Nacimientos <- data.frame(ID = 1:31243) %>%
 Hijos_naci <- seq(as.Date('1926/01/01'), as.Date('1935/12/31'), by="day")
 pesos_hijo <- dbinom(prob = 1/2,x = 1:3652,size = 3652)
 Hijos <- Nacimientos %>% 
-  sample_n(size = 50841,replace = TRUE) %>% 
+  sample_n(size = 8141,replace = TRUE) %>% 
   select(ID) %>% 
-  mutate(Nacimiento_Hijo = sample(Hijos_naci,size = 50841,prob = pesos_hijo,replace = TRUE)) %>% 
+  mutate(Nacimiento_Hijo = sample(Hijos_naci,size = 8141,prob = pesos_hijo,replace = TRUE)) %>% 
   arrange(ID) %>% 
-  mutate(Nacimiento_Hijo = as.character(Nacimiento_Hijo))
+  mutate(Nacimiento_Hijo = as.character(Nacimiento_Hijo)) %>% 
+  separate(col = Nacimiento_Hijo,sep = '-',into = c('Anno','Mes','Dia'),convert = FALSE) %>% 
+  rowwise() %>% 
+  mutate(Nacimiento_Hijo = paste(Dia,Mes,Anno,sep ='-')) %>% 
+  ungroup() %>% 
+  select(ID,Nacimiento_Hijo) %>% 
+  sample_n(size = 8141,replace = FALSE)
 
 
 
 
 Fallecimientos <- Nacimientos %>% 
-  select(ID,Anno_Muerte,Mes_Muerte,Dia_Muerte,Hora_Muerte,Minuto_Muerte)
+  select(ID,Anno_Muerte,Mes_Muerte,Dia_Muerte,Hora_Muerte,Minuto_Muerte) %>% 
+  mutate(Anno_Muerte = as.integer(Anno_Muerte),
+         Mes_Muerte = as.integer(Mes_Muerte),
+         Dia_Muerte = as.integer(Dia_Muerte))
 
 
 Nacimientos <- Nacimientos %>% 
